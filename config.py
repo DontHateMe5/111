4694ea258b0d8c03c306bbfab1e09199375d5941
DontHateMe5 <137700116+DontHateMe5@users.noreply.github.com>
2023-06-25 15:58:40 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+#By кот хакер 1234#
+#Downloaded from TG @top_softs
+#Leave this inscription#
+
+token='6103055351:AAGWapn4rJuM__7QLIz34A7m25hxv53Ix_w' #https://t.me/BotFather 
+admin_id=int(102952632) #https://t.me/getidsbot 
+
